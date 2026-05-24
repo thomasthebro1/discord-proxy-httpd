4336c5645303805f0ffb525cace319b2df1d2c74
thomasthebro1 <thomasthebro1@gmail.com>
2026-05-24 22:34:47 +0100

    refactor: clean up codebase and dependencies

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,7 +1,9 @@
 import { preHandlerHookHandler } from 'fastify';
+import { PrismaClient } from '../lib/prisma/client.ts';
 
 declare module 'fastify' {
   interface FastifyInstance {
+    prisma: PrismaClient;
     authenticate: preHandlerHookHandler;
   }
 }
